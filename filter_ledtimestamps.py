866c97d288d4b6d714aa89cf3f0ef081fa3c0ed7
mnrojas2 <mnrojas2@uc.cl>
2024-05-29 11:34:33 -0400

    remove specific data files.

--- filter_ledtimestamps.py
+++ filter_ledtimestamps.py
@@ -8,30 +8,8 @@ from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser(description='Filters the timestamp where the leds blinked according to the system.')
 parser.add_argument('vidname', type=str, help='Name of video (mp4 format).')
+parser.add_argument('-p', '--plot', action='store_true', default=False, help='Show plots from the process.')
 
-# Main
-args = parser.parse_args()
-
-# Output logged files
-logfiles = {
-    "C0008": "log_output_0429_095517.txt",
-    "C0009": "log_output_0430_131016.txt",
-    "C0010": "log_output_0430_134403.txt",
-    "C0011": "log_output_0430_142543.txt",
-    "C0012": "log_output_0430_153146.txt"
-}
-
-# From comparing videos to the plots, we can find out when the first green led blinks seen on video are in the timescale of the logfile.
-# tuple = (number of times the green led blinked before being recorded on video (logfile data), number of times the led blinked before the first green (on video))
-v_blink = {
-    "C0008": (1, 2),
-    "C0009": (1, 2),
-    "C0010": (1, 0),
-    "C0011": (1, 1),
-    "C0012": (1, 0)
-}
-
-data, header = droneData.parse_source_logfile(f"info/{logfiles[args.vidname]}", show_labels=False)
 
 def fixLedType(ledtype):
     # Fix a temporal issue with the green led count in the logfiles
@@ -39,73 +17,110 @@ def fixLedType(ledtype):
     ledtype[f_g::5] = 2
     return ledtype
 
-# Get data from logfile
-t = data["DateTimeRPI"]
 
-altitude = data["Altitude"]
-led_time = data["LastLedCamTime"]
-led_type = data["LedCamType"]
+# Main
+def main():
+    args = parser.parse_args()
 
-# Remove empty rows
-t_ft = t[led_time > 0]
-altitude_ft = altitude[led_time > 0]
-led_time_ft = led_time[led_time > 0]
-led_type_ft = led_type[led_time > 0]
+    # Output logged files
+    logfiles = {
+        "C0008": "log_output_0429_095517.txt",
+        "C0009": "log_output_0430_131016.txt",
+        "C0010": "log_output_0430_134403.txt",
+        "C0011": "log_output_0430_142543.txt",
+        "C0012": "log_output_0430_153146.txt"
+    }
 
-# Fix green blink in logfile data
-led_type_ft = fixLedType(led_type_ft)
+    # From comparing videos to the plots, we can find out when the first green led blinks seen on video are in the timescale of the logfile.
+    # tuple = (number of times the green led blinked before being recorded on video (logfile data), number of times the led blinked before the first green (on video))
+    v_blink = {
+        "C0008": (1, 2),
+        "C0009": (1, 2),
+        "C0010": (1, 0),
+        "C0011": (1, 1),
+        "C0012": (1, 0)
+    }
+
+    data, _ = droneData.parse_source_logfile(f"info/{logfiles[args.vidname]}", show_labels=False)
+
+    # Get data from logfile
+    t = data["DateTimeRPI"]
+    altitude = data["Altitude"]
+    led_time = data["LastLedCamTime"]
+    led_type = data["LedCamType"]
+
+    # Adjust all columns removing the rows where are empty values in LastLedCamTime or LedCamType
+    t_ft = t[led_time > 0]
+    altitude_ft = altitude[led_time > 0]
+    led_time_ft = led_time[led_time > 0]
+    led_type_ft = led_type[led_time > 0]
+
+    # Fix green blink in logfile data (temporal)
+    led_type_ft = fixLedType(led_type_ft)
+
+    # Plotting
+    # First plot
+    if args.plot:
+        # Create samples for x-axis
+        smp = np.arange(altitude_ft.shape[0])
+
+        #   First subplot
+        fig, ax1 = plt.subplots()
+        ax1.plot(smp, altitude_ft, 'b-')
+        ax1.set_xlabel('samples')
+        ax1.set_ylabel('altitude', color='b')
+
+        #   Second subplot with its own y-axis
+        ax2 = ax1.twinx()
+        ax2.plot(smp, led_type_ft, color='g', linestyle='', marker='o')
+        ax2.plot(smp, np.where(led_type_ft > 1, 1.05, 1), color='r', linestyle='', marker='o')
+        ax2.set_ylabel('led_type', color='g')
+        plt.title("Altitude & blink LEDs vs time (from logfile)")
 
 
-# Plotting
-# Create samples for x-axis
-smp = np.arange(altitude_ft.shape[0])
-# print(smp.shape, altitude_ft.shape, led_time_ft.shape, led_type_ft.shape)
+    # Get starting point from first blink seen on video
+    vid_start = 5 * v_blink[args.vidname][0] - v_blink[args.vidname][1]
 
-# First subplot
-fig, ax1 = plt.subplots()
-ax1.plot(smp, altitude_ft, 'b-')
-ax1.set_xlabel('samples')
-ax1.set_ylabel('altitude', color='b')
+    # Adjust arrays to show only values seen on video
+    t_fit = t_ft[vid_start:]
+    altitude_fit = altitude_ft[vid_start:]
+    led_time_fit = led_time_ft[vid_start:]
+    led_type_fit = led_type_ft[vid_start:]
 
-# Second subplot with its own y-axis
-ax2 = ax1.twinx()
-ax2.plot(smp, led_type_ft, color='g', linestyle='', marker='o')
-ax2.plot(smp, np.where(led_type_ft > 1, 1.05, 1), color='r', linestyle='', marker='o')
-ax2.set_ylabel('led_type', color='g')
+    # Create new samples for plotting
+    smp = np.arange(altitude_fit.shape[0])
+    # print(smp.shape, altitude_ft.shape, led_time_ft.shape, led_type_ft.shape)
 
+    # Second plot
+    if args.plot:
+        #   First subplot
+        fig, ax1 = plt.subplots()
+        ax1.plot(smp, altitude_fit, 'b-')
+        ax1.set_xlabel('samples')
+        ax1.set_ylabel('altitude', color='b')
 
-# Adjust logfile data to data from video
-vid_blinks = v_blink[args.vidname]
-vid_start = 5 * vid_blinks[0] - vid_blinks[1]
+        #   Second subplot with its own y-axis
+        ax2 = ax1.twinx()
+        ax2.plot(smp, led_type_fit, color='g', linestyle='', marker='o')
+        ax2.plot(smp, np.where(led_type_fit > 1, 1.05, 1), color='r', linestyle='', marker='o')
+        ax2.set_ylabel('led_type', color='g')
+        plt.title("Altitude & blink LEDs vs time (adjusted to video)")
 
-t_fit = t_ft[vid_start:]
-altitude_fit = altitude_ft[vid_start:]
-led_time_fit = led_time_ft[vid_start:]
-led_type_fit = led_type_ft[vid_start:]
+        # Show the plots
+        plt.show()
 
-# Create new samples for plotting
-smp = np.arange(altitude_fit.shape[0])
-# print(smp.shape, altitude_ft.shape, led_time_ft.shape, led_type_ft.shape)
+    # Get list of frames where the led was blinking
+    frame_list = [int(x[5:-4]) for x in os.listdir(f"frames/{args.vidname}/")]
+    frame_list.sort()
 
-fig, ax1 = plt.subplots()
-ax1.plot(smp, altitude_fit, 'b-')
-ax1.set_xlabel('samples')
-ax1.set_ylabel('altitude', color='b')
+    # Create an array with values covering the complete video
+    end_frames = frame_list[-1]+1
+    frames = np.arange(0, end_frames, 1)
+    
+    # Create an array with timestamp values for each frame
+    timestamps = t_fit[0] + (t_fit[-1] - t_fit[0])/(frame_list[-1] - frame_list[0]) * (frames - frame_list[0])
 
-# Create a second subplot with its own y-axis
-ax2 = ax1.twinx()
-ax2.plot(smp, led_type_fit, color='g', linestyle='', marker='o')
-ax2.plot(smp, np.where(led_type_fit > 1, 1.05, 1), color='r', linestyle='', marker='o')
-ax2.set_ylabel('led_type', color='g')
+    # Save the list of frames and timestamps
+    np.savetxt(f"./info/timelist{args.vidname}.txt", np.column_stack((frames, timestamps)), fmt='%d,%1.4f', delimiter=',', header='frame,timestamp', comments='')
 
-# Show the plot
-# plt.show()
-
-frame_list = [int(x[5:-4]) for x in os.listdir(f"frames/{args.vidname}/")]
-frame_list.sort()
-
-frames = np.arange(0, 1+frame_list[-1], 1)
-timestamps = t_fit[0] + (t_fit[-1] - t_fit[0])/(frame_list[-1] - frame_list[0]) * (frames - frame_list[0])
-
-# Save the list of frames that have the LED turned on.
-np.savetxt(f"./info/timelist{args.vidname}.txt", np.column_stack((frames, timestamps)), fmt='%d,%1.4f', delimiter=',', header='frame,timestamp', comments='')
+if __name__=='__main__': main()
